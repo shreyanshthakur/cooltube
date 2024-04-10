22ed3ee3b19d50b527d564197f55ee2ab6d2eda4
Shreyansh Singh Thakur <thakur.shreyansh11@gmail.com>
2024-04-10 15:19:56 +0530

    Populated teh video container with videos from Youtube API

--- src/Components/VideoCard.js
+++ src/Components/VideoCard.js
@@ -6,12 +6,12 @@ const VideoCard = ({ info }) => {
   const { channelTitle, title, thumbnails } = snippet;
 
   return (
-    <div>
-      <img alt="thumbnail" src={thumbnails.medium.url} />
+    <div className="p-2 m-2 w-72 shadow-lg">
+      <img className="rounded-lg" alt="thumbnail" src={thumbnails.medium.url} />
       <ul>
-        <li>{title}</li>
+        <li className="font-bold py-2">{title}</li>
         <li>{channelTitle}</li>
-        <li>{statistics.viewCount}</li>
+        <li>{statistics.viewCount} views</li>
       </ul>
     </div>
   );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 const API_KEY = "AIzaSyBS8_S3Y9maFp7jQkbkdanxAHQepFwrpN8";
 
 export const YOUTUBE_VIDEOS_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&chart=mostPopular&key=" +
+  "https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   API_KEY;
